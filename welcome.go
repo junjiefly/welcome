68291c1a3589c5907be86af2feecc6821077f5dc
junjiefly <107779959+junjiefly@users.noreply.github.com>
2023-08-12 17:22:06 +0800

    Update welcome.go

--- welcome.go
+++ welcome.go
@@ -3,8 +3,8 @@ package github.com/junjiefly/welcome
 import "fmt"
 
 const welcome = "======================================================" +
-	"you are using a tool from https://github.com/junjiefly, " +
-	"please visit this repository in your spare time and give it a star. " +
+	"You are using a tool from https://github.com/junjiefly. " +
+	"Please visit this repository in your spare time and give it a star. " +
 	"The author will be extremely grateful. Thank you." +
 	"======================================================"
 
