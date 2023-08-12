f206dc6646697c7d55d22a317d0730f873069661
junjiefly <107779959+junjiefly@users.noreply.github.com>
2023-08-12 17:21:43 +0800

    Update welcome.go

--- welcome.go
+++ welcome.go
@@ -5,7 +5,8 @@ import "fmt"
 const welcome = "======================================================" +
 	"you are using a tool from https://github.com/junjiefly, " +
 	"please visit this repository in your spare time and give it a star. " +
-	"The author will be extremely grateful. Thank you."
+	"The author will be extremely grateful. Thank you." +
+	"======================================================"
 
 func Welcome() {
 	fmt.Println(welcome)
